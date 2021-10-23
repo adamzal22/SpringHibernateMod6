b7dd92ce3aa3951b3aedd972b39bf88e67c12937
adamzal22@gmail.com <Kanon1425>
2021-10-23 10:10:19 +0200

    Initial commit

---
+++ src/main/java/pl/coderslab/springhibernatemod6/SpringHibernateMod6Application.java
@@ -0,0 +1,13 @@
+package pl.coderslab.springhibernatemod6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateMod6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHibernateMod6Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/springhibernatemod6/SpringHibernateMod6ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.springhibernatemod6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateMod6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
